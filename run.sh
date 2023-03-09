1bb44c17daa61caf3f86b2f4914e3be05cdfd01a
Ismaellamgadar <90750867+Ismaellamgadar@users.noreply.github.com>
2023-03-09 09:39:51 +0100

    ok

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+docker login --username ismaellamgadar --password ${{secrets.DOCKER_PASSWORD}}
 docker build -t backend .
 docker tag backend ismaellamgadar/backend
 docker push ismaellamgadar/backend
